e7a48441961d9a9d52a8ac806cc7bd436a8b7bad
EduardoG0202 <156858517+EduardoG0202@users.noreply.github.com>
2024-06-03 17:09:53 -0300

    Starting to play with some ideas

--- CoffeeMachine.php
+++ CoffeeMachine.php
@@ -1,1 +1,13 @@
 <?php
+
+/* Goal: To make a functional coffee machine, with not DB link
+Use NumbrSelect to define which type of coffee will be made, then use the correct function
+Functions need to have the recipe of the Desired coffee, and a function to determine the if the insert value is correct
+to the amount nedded
+*/
+
+class CoffeMachine ();
+public $NumbrSelect="";
+public $InsertValue="";
+
+
