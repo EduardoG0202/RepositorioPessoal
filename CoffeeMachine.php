1f9004a8f1bc2584edf9bc9eeb74d26eb47cce37
EduardoG0202 <156858517+EduardoG0202@users.noreply.github.com>
2024-06-04 17:20:21 -0300

    Starting to write the Select Function

--- CoffeeMachine.php
+++ CoffeeMachine.php
@@ -6,8 +6,14 @@ Functions need to have the recipe of the Desired coffee, and a function to deter
 to the amount nedded
 */
 
-class CoffeMachine ();
-public $NumbrSelect="";
-public $InsertValue="";
+class CoffeMachine (){
+    public $nmbrSelected="";
+    public $insertValue="";
+    public $number = [1 , 2 , 3 , 4 , 5 , 6];
+    public $value = ;
 
+    public function SelectNmbr($Number){
+        
+    }
+}
 
